f7fbe23628d41b574043f83b2aac482378a262b4
bharatvarmagit <itisbharatvarma@gmail.com>
2021-01-08 10:58:18 +0530

    added jsonwebtoken , need to add users with username and hashed passwords in postgres

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -3,30 +3,18 @@ const jwt = require('jsonwebtoken')
 
 const router = require('express').Router();
 
-router.post('/', verifyToken, (req, res) => {
+router.post('/', (req, res) => {
     const user ={
         id:1,
         username: 'bharat',
     }
     jwt.sign({user}, 'secretkey', (err, token) => {
         res.json({token})
-        req
     })
 
 });
 
-function verifyToken(req, res, next) {
-    const bearerHeader = req.headers['authorization'];
-    if (bearerHeader){
-        const bearer = bearerHeader.split(' ');
-        const bearerToken = bearer[1];
-        req.token = bearerToken;
-        next();
-    }
-    else{
-        res.sendStatus(403)
-    }
-}
+
 
 module.exports = router;
-module.exports.verifyToken = verifyToken;
+
--- src/routes/customers.js
+++ src/routes/customers.js
@@ -2,19 +2,27 @@ const express = require('express');
 const router = express.Router();
 const {db} = require('../config/database')
 const {Customer} = require('../models/customer')
-const {verifyToken} = require('../routes/auth')
 const jwt = require('jsonwebtoken')
 
 //get all customers
 router.get('/', verifyToken,  (req, res) => {
-       Customer.findAll()
-        .then(data=>{
-            console.log("cusomters are :\n \t",data);
-            let myres=[];
-            data.forEach(row=>myres.push(row.firstName))
-            res.send(myres)
-        }).catch(err=>{
-            console.log("encountered error",err)
+        jwt.verify(req.token, 'secretkey', (err, authData) => {
+            if (err) res.sendStatus(403);
+            else{
+                Customer.findAll()
+                    .then(data=>{
+                        console.log("cusomters are :\n \t",data);
+                        let myres=[];
+                        data.forEach(row=>myres.push(row.firstName))
+                        res.json({
+                            data: myres,
+                            authData
+                        })
+                    })
+                    .catch(err=>{
+                        console.log("encountered error",err)
+                    })
+            }
         })
 });
 //add customer
@@ -29,4 +37,18 @@ router.get('/', verifyToken,  (req, res) => {
 //         .catch(err => console.log(err))
 
 // })
+
+function verifyToken(req, res, next) {
+    const bearerHeader = req.headers['authorization'];
+    if (typeof bearerHeader !== 'undefined'){
+        const bearer = bearerHeader.split(' ');
+        const bearerToken = bearer[1];
+        req.token = bearerToken;
+        next();
+    }
+    else{
+        console.log("no bearer token in verify token")
+        res.sendStatus(403);
+    }
+}
 module.exports = router;
